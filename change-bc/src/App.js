9f8a3b770680b197c458c7b9855091e89b6eadef
elzeveta <eliza.amanbaeva@gmail.com>
2021-06-01 01:31:53 -0700

    Added components and router

--- change-bc/src/App.js
+++ change-bc/src/App.js
@@ -5,23 +5,33 @@ import {
   Route,
   Link
 } from "react-router-dom";
+import Simple from "./components/Simple"
+import Hex from "./components/Hex"
 import './App.css';
 
-class App extends Component {
-  constructor(){
-    super();
-    this.state = {
-
-    }
-  }
-  render() {
+export default function App() {
     return (
-      <div className="App">
-  
+      <Router>
+      <div>
+        <ul className="App">
+          <li>
+            <Link to="/Simple">Simple</Link>
+          </li>
+          <li>
+            <Link to="/Hex">Hex</Link>
+          </li>
+        </ul>
+        <Switch>
+          <Route path="/Simple">
+            <Simple/>
+          </Route>
+          <Route path="/Hex">
+            <Hex/>
+          </Route>
+        </Switch>
       </div>
+      </Router>
     );
   }
 
-}
 
-export default App;
